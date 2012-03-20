0b89feaf42e2ff5160b142329f563dd88cbe9d57
Joseph Bell <jbell@numerex.com>
2012-03-20 12:14:07 -0500

    rubyracer for Javascript runtime, additional mojo in config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,10 @@
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "bundler/capistrano"
+require "rvm/capistrano"
+set :rvm_ruby_string, '1.9.2'
+set :rvm_type, :user
+
 #========================
 #CONFIG
 #========================
